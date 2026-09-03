0e1079ca6044d97229a503a031eecb8b04874262
Nargis Fatima <nargis@modfxmedia.com>
2026-09-04 02:23:59 +0500

    typ pixel

--- app/(routes)/lp/p-long/thank-you/page.tsx
+++ app/(routes)/lp/p-long/thank-you/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { buildMetadata } from "@/lib/metadata";
 import LPHeader from "@/components/lp/LPHeader";
 import LPFooter from "@/components/lp/LPFooter";
@@ -15,6 +16,33 @@ export const metadata: Metadata = buildMetadata({
 export default function PLongThankYouPage() {
   return (
     <>
+      {/* Meta Pixel Code */}
+      <Script id="meta-pixel" strategy="afterInteractive">
+        {`!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '926054265492584');
+fbq('track', 'PageView');`}
+      </Script>
+      <noscript>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id=926054265492584&ev=PageView&noscript=1"
+          alt=""
+        />
+      </noscript>
+      {/* End Meta Pixel Code */}
+      <Script id="meta-pixel-lead" strategy="afterInteractive">
+        {`fbq('track', 'Lead');`}
+      </Script>
       <LPHeader />
       <ThankYouContent />
       <MapSection hidePhone />
